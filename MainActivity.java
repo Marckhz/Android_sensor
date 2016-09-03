90ad0339a9bb7f85f34a132b354d824a0b8b0891
Marco Hernandez <marcohdes94i@gmail.com>
2016-09-03 15:41:05 -0600

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package com.example.marco.sensor;
 
 import android.app.Activity;
 import android.app.usage.UsageEvents;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,6 +12,10 @@ import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.PrintWriterPrinter;
+import android.util.Printer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -20,8 +25,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.CharArrayWriter;
+import java.io.DataOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -46,42 +53,46 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Socket socket;
     private float[] arraySensor = new float[3];
     private String socketOutput;
-    private OutputStream outputStream;
+    private  OutputStream outStream;
+
+
 
 
     public MainActivity() {
 
-
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         texto = (TextView) findViewById(R.id.textView);
         texto1 = (TextView) findViewById(R.id.textView2);
         texto2 = (TextView) findViewById(R.id.textView3);
-
         new Thread(new SocketThread()).start();
     }
     class SocketThread implements Runnable {
 
-            @Override
-            public void run() {
-                try {
-                    socket = new Socket("192.168.0.6", 12349);
-                }
-                catch (UnknownHostException b){
-                    b.printStackTrace();
-                }
-                catch (IOException b1){
-                    b1.printStackTrace();
-                }
 
-            }
+        @Override
+            public void run() {
+
+
+                try {
+                    socket = new Socket("192.168.0.6", 5000);
+                    PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+                    out.print(socket);
+
+
+
+                    //out.println(socketOutput);
+
+                }
+                catch (java.io.IOException e){
+                    e.printStackTrace();
+                }
+        }
     }
 
     @Override
@@ -109,23 +120,26 @@ public class MainActivity extends Activity implements SensorEventListener {
         texto1.setText(Float.toString(event.values[1]));
         texto2.setText(Float.toString(event.values[2]));/* cambio Float.toString por String.valueO */
 
-        arraySensor[0] = event.values[0];
-        arraySensor[1] = event.values[1];
-        arraySensor[2] = event.values[2];
-
-        socketOutput = (Float.toString(event.values[0]));
+        arraySensor[0] = event.values[0];// Introducir todo esto en un solo string
+        arraySensor[1] = event.values[1];// Introducir todo esto en Oun solo String
+        arraySensor[2] = event.values[2];// Introducir todo en un solo stri
 
         try {
-            PrintWriter output = new PrintWriter(new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream())), true);
-            output.println(socketOutput);
-        }catch (java.io.IOException b) {
-                b.printStackTrace();
+            socketOutput = Float.toString(arraySensor[0]);
+
+            OutputStream outStream = socket.getOutputStream();
+            PrintWriter out = new PrintWriter(outStream);
+
+            out.print(socketOutput);
+
+        }catch (java.io.IOException e){
+            e.printStackTrace();
         }
-
-
     }
+
+
 }
+
 
 
 
